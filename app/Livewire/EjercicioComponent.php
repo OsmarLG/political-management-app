1dc6c05464b2ff8a7bc49bb067267d4ec5de0d16
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2024-03-19 19:14:57 -0700

    AVANCE EJERCICIO

--- app/Livewire/EjercicioComponent.php
+++ app/Livewire/EjercicioComponent.php
@@ -2,16 +2,21 @@
 
 namespace App\Livewire;
 
-use App\Models\Configuracion;
+use App\Models\User;
+use App\Models\Manzana;
+use App\Models\Seccion;
 use Livewire\Component;
 use App\Models\Encuesta;
 use Filament\Forms\Form;
 use App\Models\Ejercicio;
+use App\Models\Configuracion;
 use App\Models\EncuestaOpcion;
 use App\Models\EncuestaPregunta;
 use App\Models\EncuestaRespuesta;
+use App\Models\UsuarioAsignacion;
 use Illuminate\Contracts\View\View;
 use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Components\TextInput;
@@ -81,29 +86,60 @@ class EjercicioComponent extends Component implements HasForms
     ->helperText('Coordenadas para el ejercicio.')
     ->readOnly()    ;
 
-/*
-    $campos[] =        
-    Select::make('manzana_id')
-    ->label('Sección')
-    ->options(function (callable $get) {
-        $zonaId = $get('zona_id');
-        // Si no se ha seleccionado una zona, devuelve todas las secciones.
-        // De lo contrario, filtra las secciones por la zona seleccionada.
-        return Seccion::when($zonaId, function ($query) use ($zonaId) {
-            return $query->where('zona_id', $zonaId);
-        })->pluck('nombre', 'id');
-    })
-    ->searchable()
-    ->preload()
-    ->reactive() // Importante para asegurar que se actualiza cuando cambia zona_id
-    ->required()
-    ->label('Sección')
-    ->placeholder('Selecciona una sección')
-    ->columnSpan(2);
-*/
 
+    $usuario = User::find(auth()->user()->id);
 
+    if($usuario->hasRole('MANZANAL')){
 
+        $usuario = User::find(auth()->user()->id);
+        $manzana_id = UsuarioAsignacion::where('user_id',$usuario->id)->where('modelo','Manzana')->first()->id_modelo;
+        $manzana = Manzana::where('id',$manzana_id)->pluck('nombre','id');
+
+        $campos[] =     
+        Select::make('manzana_id')
+        ->label('Manzana')
+        ->options(function (callable $get) {
+            $usuario = User::find(auth()->user()->id);
+            $manzana_id = UsuarioAsignacion::where('user_id',$usuario->id)->where('modelo','Manzana')->first()->id_modelo;
+            return $manzana = Manzana::where('id',$manzana_id)->pluck('nombre','id');
+        })
+        ->searchable()
+        ->preload()
+        ->reactive() // Importante para asegurar que se actualiza cuando cambia zona_id
+        ->required()
+        ->placeholder('Selecciona una sección');
+        
+
+    }else{
+        $campos[] =        
+        Select::make('manzana_id')
+        ->label('Sección')
+        ->options(function (callable $get) {
+    
+            $usuario = User::find(auth()->user()->id);
+            //Si el usuario es manzanal
+            if($usuario->hasRole('MANZANAL')){
+                $manzana_id = UsuarioAsignacion::where('user_id',$usuario->id)->where('modelo','Manzana')->first()->id_modelo;
+                return Manzana::where('id',$manzana_id)->pluck('nombre','id');
+            }else{
+                $zonaId = $get('zona_id');
+                // Si no se ha seleccionado una zona, devuelve todas las secciones.
+                // De lo contrario, filtra las secciones por la zona seleccionada.
+                return Seccion::when($zonaId, function ($query) use ($zonaId) {
+        
+                    return $query->where('zona_id', $zonaId);
+                })->pluck('nombre', 'id');
+            }
+    
+        })
+        ->searchable()
+        ->preload()
+        ->reactive() // Importante para asegurar que se actualiza cuando cambia zona_id
+        ->required()
+        ->label('Sección')
+        ->placeholder('Selecciona una sección')
+        ->columnSpan(2);
+    }
 
     return $form
     ->schema([
