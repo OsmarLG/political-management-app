1897039b910d5e8f17d517039639706bda84cfe2
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2024-03-21 00:02:08 -0700

    avance

--- app/Filament/Resources/EjercicioResource.php
+++ app/Filament/Resources/EjercicioResource.php
@@ -7,13 +7,18 @@ use Filament\Tables;
 use Filament\Forms\Form;
 use App\Models\Ejercicio;
 use Filament\Tables\Table;
+use App\Models\EncuestaOpcion;
+use App\Models\EncuestaPregunta;
 use Filament\Resources\Resource;
 use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\EjercicioResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\EjercicioResource\RelationManagers;
+use App\Models\EncuestaRespuesta;
 
 class EjercicioResource extends Resource
 {
@@ -27,23 +32,47 @@ class EjercicioResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                TextInput::make('folio')
-                ->label('Folio')
-                ->placeholder('folio')
-                ->required()
-                ->maxLength(255),
+        $preguntas = EncuestaPregunta::all();
 
+        // Define un array para almacenar los campos del formulario
+        $campos = [];
+        $manzana_id = null;
+    
+        // Itera sobre las preguntas y agrega los campos correspondientes al array
+        foreach ($preguntas as $pregunta) {
+            //agregamos las opciones
+            $opciones = EncuestaOpcion::where('pregunta_id',$pregunta->id)->get();
+            if($pregunta->opciones){
+                foreach ($opciones as $opcion) {
+                    // Agrega la opción al array de opciones del campo de Radio
+                    $opcionesCampo[$opcion->texto_opcion] = $opcion->texto_opcion;
+                }
+                $campo = Radio::make($pregunta->id)->options($opcionesCampo)
+                
+                ->label($pregunta->texto_pregunta)->required();
+                $campos[] = $campo;
+            }
+            $opcionesCampo = array();
+        }
+
+            $campos[] = 
+            TextInput::make('folio')
+            ->label('Folio')
+            ->placeholder('folio')
+            ->required()
+            ->maxLength(255);
+
+            $campos[] = 
                 TextInput::make('manzana.nombre')
-                ->label('Manzana')
-                ->placeholder('Manzana')
-                ->required()
-                ->formatStateUsing(function ($state, $record) {
-                    return $record->manzana->nombre ?? 'Sin manzana';
-                }) 
-                ->maxLength(255),
+            ->label('Manzana')
+            ->placeholder('Manzana')
+            ->required()
+            ->formatStateUsing(function ($state, $record) {
+                return $record->manzana->nombre ?? 'Sin manzana';
+            }) 
+            ->maxLength(255);
 
+            $campos[] = 
                 TextInput::make('latitud')
                 ->label('Latitud')
                 ->placeholder('latitud')
@@ -51,18 +80,33 @@ class EjercicioResource extends Resource
                     return $record->asignacionGeografica->latitud ?? 'Sin latitud';
                 }) 
                 ->required()
-                ->maxLength(255),
+                ->maxLength(255);
 
-                TextInput::make('longitud')
+            $campos[] = 
+                TextInput::make('latitud')
+                ->label('Latitud')
+                ->placeholder('latitud')
+                ->formatStateUsing(function ($state, $record) {
+                    return $record->asignacionGeografica->latitud ?? 'Sin latitud';
+                }) 
+                ->required()
+                ->maxLength(255);
+
+                      $campos[] = 
+                                      TextInput::make('longitud')
                 ->label('Longitud')
                 ->placeholder('Longitud')
                 ->formatStateUsing(function ($state, $record) {
                     return $record->asignacionGeografica->longitud ?? 'Sin latitud';
                 }) 
                 ->required()
-                ->maxLength(255),
+                ->maxLength(255);
 
-            ]);
+        return $form
+        ->schema([
+            Section::make('Encuesta')
+            ->schema($campos)
+        ]);
     }
 
     public static function table(Table $table): Table
